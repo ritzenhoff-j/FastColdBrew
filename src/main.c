47cd575c65cc24fc22dbd758e9259b93a6b6d3e0
ritzenhoff-j <ritzenhoff.j@husky.neu.edu>
2018-11-01 18:11:19 -0400

    Fully functional GPIO initialization

--- src/main.c
+++ src/main.c
@@ -26,26 +26,26 @@ struct PinLocation {
 // Reference the entire PinLocation struct just as a PinLocation
 typedef struct PinLocation PinLocation;
 
-static const PinLocation VacuumMotor = { GPIOA, 0 };
-static const PinLocation RecircMotor = { GPIOA, 5};
-static const PinLocation MixingMotor = { GPIOA, 6};
-static const PinLocation RecircSol = { GPIOA, 8 };
-static const PinLocation ToVacChamberSol = { GPIOA, 9 };
-static const PinLocation CoffeeReleaseSol = { GPIOA, 10 };
-static const PinLocation SonarSensor = { GPIOA, 11 };
-static const PinLocation TempSensor = { GPIOA, 12 };
-static const PinLocation PeltierSwitch = { GPIOA, 13 };
-static const PinLocation LowWaterLED = { GPIOB, 3 };
-static const PinLocation BrewTimeLED = { GPIOB, 4 };
-static const PinLocation CoolingOnLED = { GPIOB, 5 };
-static const PinLocation PressureOnLED = { GPIOB, 6 };
+static const PinLocation VacuumMotor = { GPIOA, GPIO_Pin_0 };
+static const PinLocation RecircMotor = { GPIOA, GPIO_Pin_5};
+static const PinLocation MixingMotor = { GPIOA, GPIO_Pin_6};
+static const PinLocation RecircSol = { GPIOA, GPIO_Pin_8 };
+static const PinLocation ToVacChamberSol = { GPIOA, GPIO_Pin_9 };
+static const PinLocation CoffeeReleaseSol = { GPIOA, GPIO_Pin_10 };
+static const PinLocation SonarSensor = { GPIOA, GPIO_Pin_11 };
+static const PinLocation TempSensor = { GPIOA, GPIO_Pin_12 };
+static const PinLocation PeltierSwitch = { GPIOA, GPIO_Pin_13 };
+static const PinLocation LowWaterLED = { GPIOB, GPIO_Pin_3 };
+static const PinLocation BrewTimeLED = { GPIOB, GPIO_Pin_4 };
+static const PinLocation CoolingOnLED = { GPIOB, GPIO_Pin_5 };
+static const PinLocation PressureOnLED = { GPIOB, GPIO_Pin_6 };
 
-static const PinLocation SmallButton = { GPIOB, 12 };
-static const PinLocation MediumButton = { GPIOB, 13};
-static const PinLocation LargeButton = { GPIOB, 14 };
+static const PinLocation SmallButton = { GPIOB, GPIO_Pin_12 };
+static const PinLocation MediumButton = { GPIOB, GPIO_Pin_13};
+static const PinLocation LargeButton = { GPIOB, GPIO_Pin_14 };
 
-static const PinLocation RxPin = { GPIOA, 2 };
-static const PinLocation TxPin = { GPIOA, 3 };
+static const PinLocation RxPin = { GPIOA, GPIO_Pin_2 };
+static const PinLocation TxPin = { GPIOA, GPIO_Pin_3 };
 
 
 
@@ -69,33 +69,56 @@ static const PinLocation *inputPins[NUMBER_OF_INPUT_PINS] = {
 
 // Array of all ports utilized for GPIO initialization
 #define NUMBER_OF_PORTS 2
-static const GPIO_TypeDef knownUsedPorts[NUMBER_OF_PORTS] = { GPIOA, GPIOB };
+static const GPIO_TypeDef * knownUsedPorts[NUMBER_OF_PORTS] = { GPIOA, GPIOB };
 
 void initializeAllPins();
-void initializeGPIO(GPIO_TypeDef GPIOx, uint16_t numberOfPins, PinLocation locations[],
+void initializeGPIO(GPIO_TypeDef* GPIOx, uint16_t numberOfPins, PinLocation locations[],
 		GPIOSpeed_TypeDef speed, GPIOMode_TypeDef mode, GPIOOType_TypeDef type, GPIOPuPd_TypeDef pushPull);
-uint32_t getPortRCC_Constant(GPIO_TypeDef GPIOx);
+
+uint32_t getPortRCC_Constant(GPIO_TypeDef * GPIOx);
+GPIO_TypeDef* getPort(GPIO_TypeDef * GPIOx);
 uint8_t sameGPIO_TypeDef(GPIO_TypeDef* def1, GPIO_TypeDef* def2);
 
 
 int main(void) {
 	// SystemInit() call from mcu sets the clock speed
 
+	SystemInit();
+
+	/*
+	uint32_t RCC_AHBPeriph = getPortRCC_Constant(LowWaterLED.port);
+	uint32_t RCC_AHB_Actual = RCC_AHB1Periph_GPIOB;
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB_Actual, ENABLE);
+
+
+	GPIO_InitTypeDef currentPort_InitStruct;
+
+	currentPort_InitStruct.GPIO_Pin = GPIO_Pin_3;
+	currentPort_InitStruct.GPIO_Speed = GPIO_Speed_50MHz; // 50 MHZ clock frequency
+	currentPort_InitStruct.GPIO_Mode = GPIO_Mode_OUT; // Set GPIO output
+	currentPort_InitStruct.GPIO_OType = GPIO_OType_PP ; // Push pull mod
+	currentPort_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL; // Pull up
+
+	int actual = GPIOB;
+	int found = LowWaterLED.port;
+
+
+	GPIO_Init(found, &currentPort_InitStruct);
+	*/
+
+
 	// Initialize all GPIO pins
 	initializeAllPins();
+
 
 	uint8_t bool = 0;
 
 	while(1) {
 
 		if(bool) {
+			GPIO_ResetBits(LowWaterLED.port, LowWaterLED.pin);
 
-			if(sameGPIO_TypeDef(LowWaterLED.port, GPIOB)) {
-
-				int port = LowWaterLED.pin;
-
-				GPIO_ResetBits(LowWaterLED.port, LowWaterLED.pin);
-			}
 			bool = 0;
 		}
 		else {
@@ -107,7 +130,7 @@ int main(void) {
 
 		int j = 0;
 
-		for(int i = 0; i < 50000; i++) {
+		for(int i = 0; i < 5000000; i++) {
 			j++;
 		}
 
@@ -117,12 +140,14 @@ int main(void) {
 }
 
 
-uint32_t getPortRCC_Constant(GPIO_TypeDef GPIOx) {
-	if(sameGPIO_TypeDef(&GPIOx, GPIOA)) return RCC_AHB1Periph_GPIOA;
-	if(sameGPIO_TypeDef(&GPIOx, GPIOB)) return RCC_AHB1Periph_GPIOB;
-	if(sameGPIO_TypeDef(&GPIOx, GPIOC)) return RCC_AHB1Periph_GPIOC;
-	if(sameGPIO_TypeDef(&GPIOx, GPIOD)) return RCC_AHB1Periph_GPIOD;
-	if(sameGPIO_TypeDef(&GPIOx, GPIOE)) return RCC_AHB1Periph_GPIOE;
+uint32_t getPortRCC_Constant(GPIO_TypeDef * GPIOx) {
+	GPIO_TypeDef* port = getPort(GPIOx);
+
+	if(port == GPIOA) return RCC_AHB1Periph_GPIOA;
+	if(port == GPIOB) return RCC_AHB1Periph_GPIOB;
+	if(port == GPIOC) return RCC_AHB1Periph_GPIOC;
+	if(port == GPIOD) return RCC_AHB1Periph_GPIOD;
+	if(port == GPIOE) return RCC_AHB1Periph_GPIOE;
 //	if(sameGPIO_TypeDef(&GPIOx, GPIOF)) return RCC_AHB1Periph_GPIOF;
 //	if(sameGPIO_TypeDef(&GPIOx, GPIOG)) return RCC_AHB1Periph_GPIOG;
 //	if(sameGPIO_TypeDef(&GPIOx, GPIOH)) return RCC_AHB1Periph_GPIOH;
@@ -131,24 +156,42 @@ uint32_t getPortRCC_Constant(GPIO_TypeDef GPIOx) {
 	return 0;
 }
 
+GPIO_TypeDef* getPort(GPIO_TypeDef* GPIOx) {
+	if(sameGPIO_TypeDef(GPIOx, GPIOA)) return GPIOA;
+	if(sameGPIO_TypeDef(GPIOx, GPIOB)) return GPIOB;
+	if(sameGPIO_TypeDef(GPIOx, GPIOC)) return GPIOC;
+	if(sameGPIO_TypeDef(GPIOx, GPIOD)) return GPIOD;
+	if(sameGPIO_TypeDef(GPIOx, GPIOE)) return GPIOE;
+//	if(sameGPIO_TypeDef(GPIOx, GPIOF)) return GPIOF;
+//	if(sameGPIO_TypeDef(GPIOx, GPIOG)) return GPIOG;
+//	if(sameGPIO_TypeDef(GPIOx, GPIOH)) return GPIOH;
+//	if(sameGPIO_TypeDef(GPIOx, GPIOI)) return GPIOI;
+
+	return 0;
+}
+
 void initializeAllPins() {
-	for(uint8_t portIndex = 0; portIndex < NUMBER_OF_PORTS; portIndex++) {
-		GPIO_TypeDef currentPort = knownUsedPorts[portIndex];
+	for(uint8_t portIndex = 1; portIndex < NUMBER_OF_PORTS; portIndex++) {
+
+		GPIO_TypeDef* currentPort = knownUsedPorts[portIndex];
 
 		initializeGPIO(currentPort, NUMBER_OF_OUTPUT_PINS, *outputPins,
-				GPIO_Speed_50MHz, GPIO_Mode_OUT, GPIO_OType_PP, GPIO_PuPd_UP);
+				GPIO_Speed_50MHz, GPIO_Mode_OUT, GPIO_OType_PP, GPIO_PuPd_NOPULL);
 
+		/*
 		initializeGPIO(currentPort, NUMBER_OF_INPUT_PINS, *inputPins,
-				GPIO_Speed_50MHz, GPIO_Mode_OUT, GPIO_OType_PP, GPIO_PuPd_UP);
+				GPIO_Speed_50MHz, GPIO_Mode_IN, GPIO_OType_PP, GPIO_PuPd_NOPULL);
+		*/
 	}
 }
-void initializeGPIO(GPIO_TypeDef GPIOx, uint16_t numberOfPins, PinLocation locations[],
+void initializeGPIO(GPIO_TypeDef* GPIOx, uint16_t numberOfPins, PinLocation locations[],
 		GPIOSpeed_TypeDef speed, GPIOMode_TypeDef mode,
 		GPIOOType_TypeDef otype, GPIOPuPd_TypeDef pushPull) {
 
+	// Initialize the clock on the board
 	uint32_t RCC_AHBPeriph = getPortRCC_Constant(GPIOx);
-
 	RCC_AHB1PeriphClockCmd(RCC_AHBPeriph, ENABLE);
+
 
 	GPIO_InitTypeDef currentPort_InitStruct;
 
@@ -158,23 +201,27 @@ void initializeGPIO(GPIO_TypeDef GPIOx, uint16_t numberOfPins, PinLocation locat
 	currentPort_InitStruct.GPIO_PuPd = pushPull; // Pull up
 
 	for(uint16_t pinIndex = 0; pinIndex < numberOfPins; pinIndex++) {
-		if(sameGPIO_TypeDef(locations[pinIndex].port, &GPIOx)) {
+		PinLocation currentIO = locations[pinIndex];
+
+		if(sameGPIO_TypeDef(currentIO.port, GPIOx)) {
 			// This takes in a binary number... To use multiple LED's one needs to use a Bitwise OR operator
-			currentPort_InitStruct.GPIO_Pin |= locations[pinIndex].pin;
+			currentPort_InitStruct.GPIO_Pin |= currentIO.pin;
 		}
 	}
 
-	GPIO_Init(&GPIOx, &currentPort_InitStruct); // do the init
+	GPIO_Init(getPort(GPIOx), &currentPort_InitStruct); // do the init
 }
 
 uint8_t sameGPIO_TypeDef(GPIO_TypeDef* def1, GPIO_TypeDef* def2) {
-	return (def1->BSRRH == def2->BSRRH)
-			& (def1->BSRRL == def2->BSRRL)
-			& (def1->IDR == def2->IDR)
-			& (def1->LCKR == def2->LCKR)
-			& (def1->MODER == def2->MODER)
-			& (def1->ODR == def2->ODR)
-			& (def1->OSPEEDR == def2->OSPEEDR)
-			& (def1->OTYPER == def2->OTYPER)
-			& (def1->PUPDR == def2->PUPDR);
+	return (def1->MODER == def2->MODER);
+
+	/*(def1->BSRRH == def2->BSRRH)
+						& (def1->BSRRL == def2->BSRRL)
+						& (def1->IDR == def2->IDR)
+						& (def1->LCKR == def2->LCKR)
+
+						& (def1->ODR == def2->ODR)
+						& (def1->OSPEEDR == def2->OSPEEDR)
+						& (def1->OTYPER == def2->OTYPER)
+						& (def1->PUPDR == def2->PUPDR); */
 }
