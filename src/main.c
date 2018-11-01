62d9343784ab456d99d2f0a333fb2d829ec456b8
ritzenhoff-j <ritzenhoff.j@husky.neu.edu>
2018-10-31 20:52:31 -0400

    Made the structs work and all errors dissapear

--- src/main.c
+++ src/main.c
@@ -49,41 +49,66 @@ static const PinLocation RxPin = { GPIOA, 2 };
 static const PinLocation TxPin = { GPIOA, 3 };
 
 // Array of all pins utilized for GPIO initialization
-static const uint8_t numberOfOutputPins = 15;
-static const PinLocation outputPins[numberOfOutputPins] = {
-		VacuumMotor, RecircMotor, MixingMotor,
-		RecircSol, ToVacChamberSol, CoffeeReleaseSol,
-		SonarSensor, TempSensor,
-		PeltierSwitch,
-		LowWaterLED, BrewTimeLED, CoolingOnLED, PressureOnLED,
-		TxPin};
+#define NUMBER_OF_OUTPUT_PINS ((int)15)
+static const PinLocation *outputPins[NUMBER_OF_OUTPUT_PINS] = {
+		&VacuumMotor, &RecircMotor, &MixingMotor,
+		&RecircSol, &ToVacChamberSol, &CoffeeReleaseSol,
+		&SonarSensor, &TempSensor,
+		&PeltierSwitch,
+		&LowWaterLED, &BrewTimeLED, &CoolingOnLED, &PressureOnLED,
+		&TxPin};
 
-static const uint8_t numberOfInputPins = 4;
-static const PinLocation inputPins[numberOfInputPins] = {
-		SmallButton, MediumButton, LargeButton,
-		RxPin };
+
+#define NUMBER_OF_INPUT_PINS ((int)4)
+static const PinLocation *inputPins[NUMBER_OF_INPUT_PINS] = {
+		&SmallButton, &MediumButton, &LargeButton,
+		&RxPin };
 
 
 // Array of all ports utilized for GPIO initialization
-static const uint8_t numberOfPorts = 2;
-static const GPIO_TypeDef knownUsedPorts[numberOfPorts] = { GPIOA, GPIOB };
+#define NUMBER_OF_PORTS 2
+GPIO_TypeDef knownUsedPorts[NUMBER_OF_PORTS] = { GPIOA, GPIOB };
 
-uint32_t getPortRCC_Constant(GPIO_TypeDef GPIOx);
+void initializeAllPins();
 void initializeGPIO(GPIO_TypeDef GPIOx, uint16_t numberOfPins, PinLocation locations[],
 		GPIOSpeed_TypeDef speed, GPIOMode_TypeDef mode, GPIOOType_TypeDef type, GPIOPuPd_TypeDef pushPull);
+uint32_t getPortRCC_Constant(GPIO_TypeDef GPIOx);
+
+
+uint8_t sameGPIO_TypeDef(GPIO_TypeDef* def1, GPIO_TypeDef* def2);
+
 
 int main(void) {
 	// SystemInit() call from mcu sets the clock speed
 
+
+
 	// Initialize all GPIO pins
-	for(uint8_t portIndex = 0; portIndex < numberOfPorts; portIndex++) {
-		GPIO_TypeDef currentPort = knownUsedPorts[portIndex];
+	initializeAllPins();
 
-		initializeGPIO(currentPort, numberOfOutputPins, outputPins,
-				GPIO_Speed_50MHz, GPIO_Mode_OUT, GPIO_OType_PP, GPIO_PuPd_UP);
+	uint8_t bool = 0;
 
-		initializeGPIO(currentPort, numberOfInputPins, inputPins,
-				GPIO_Speed_50MHz, GPIO_Mode_OUT, GPIO_OType_PP, GPIO_PuPd_UP);
+	while(1) {
+
+		if(bool = 0) {
+
+			GPIO_ResetBits(&SonarSensor.port, &SonarSensor.pin);
+
+			bool = 1;
+		}
+		else {
+
+			GPIO_SetBits(&SonarSensor.port, &SonarSensor.pin);
+
+			bool = 0;
+		}
+
+		int j = 0;
+
+		for(int i = 0; i < 50000; i++) {
+			j++;
+		}
+
 	}
 
 	for(;;);
@@ -91,20 +116,30 @@ int main(void) {
 
 
 uint32_t getPortRCC_Constant(GPIO_TypeDef GPIOx) {
-	switch(GPIOx) {
-	case GPIOA: return RCC_AHB1Periph_GPIOA;
-	case GPIOB: return RCC_AHB1Periph_GPIOB;
-	case GPIOC: return RCC_AHB1Periph_GPIOC;
-	case GPIOD: return RCC_AHB1Periph_GPIOD;
-	case GPIOE: return RCC_AHB1Periph_GPIOE;
-	case GPIOF: return RCC_AHB1Periph_GPIOF;
-	case GPIOG: return RCC_AHB1Periph_GPIOG;
-	case GPIOH: return RCC_AHB1Periph_GPIOH;
-	case GPIOI:return RCC_AHB1Periph_GPIOI;
-	default: return 0x0;
-	}
+	if(sameGPIO_TypeDef(&GPIOx, GPIOA)) return RCC_AHB1Periph_GPIOA;
+	if(sameGPIO_TypeDef(&GPIOx, GPIOB)) return RCC_AHB1Periph_GPIOB;
+	if(sameGPIO_TypeDef(&GPIOx, GPIOC)) return RCC_AHB1Periph_GPIOC;
+	if(sameGPIO_TypeDef(&GPIOx, GPIOD)) return RCC_AHB1Periph_GPIOD;
+	if(sameGPIO_TypeDef(&GPIOx, GPIOE)) return RCC_AHB1Periph_GPIOE;
+	if(sameGPIO_TypeDef(&GPIOx, GPIOF)) return RCC_AHB1Periph_GPIOF;
+	if(sameGPIO_TypeDef(&GPIOx, GPIOG)) return RCC_AHB1Periph_GPIOG;
+	if(sameGPIO_TypeDef(&GPIOx, GPIOH)) return RCC_AHB1Periph_GPIOH;
+	if(sameGPIO_TypeDef(&GPIOx, GPIOI)) return RCC_AHB1Periph_GPIOI;
+
+	return 0;
 }
 
+void initializeAllPins() {
+	for(uint8_t portIndex = 0; portIndex < NUMBER_OF_PORTS; portIndex++) {
+		GPIO_TypeDef currentPort = knownUsedPorts[portIndex];
+
+		initializeGPIO(currentPort, NUMBER_OF_OUTPUT_PINS, outputPins,
+				GPIO_Speed_50MHz, GPIO_Mode_OUT, GPIO_OType_PP, GPIO_PuPd_UP);
+
+		initializeGPIO(currentPort, NUMBER_OF_INPUT_PINS, inputPins,
+				GPIO_Speed_50MHz, GPIO_Mode_OUT, GPIO_OType_PP, GPIO_PuPd_UP);
+	}
+}
 void initializeGPIO(GPIO_TypeDef GPIOx, uint16_t numberOfPins, PinLocation locations[],
 		GPIOSpeed_TypeDef speed, GPIOMode_TypeDef mode,
 		GPIOOType_TypeDef otype, GPIOPuPd_TypeDef pushPull) {
@@ -118,13 +153,24 @@ void initializeGPIO(GPIO_TypeDef GPIOx, uint16_t numberOfPins, PinLocation locat
 	currentPort_InitStruct.GPIO_OType = otype; // Push pull mod
 	currentPort_InitStruct.GPIO_PuPd = pushPull; // Pull up
 
-	for(uint16_t pinIndex = 0; pinIndex < numberOfOutputPins; pinIndex++) {
-		if(locations[pinIndex].port == GPIOx) {
+	for(uint16_t pinIndex = 0; pinIndex < numberOfPins; pinIndex++) {
+		if(sameGPIO_TypeDef(&locations[pinIndex].port, &GPIOx)) {
 			// This takes in a binary number... To use multiple LED's one needs to use a Bitwise OR operator
 			currentPort_InitStruct.GPIO_Pin |= locations[pinIndex].pin;
 		}
 	}
 
-	GPIO_Init(GPIOx, &currentPort_InitStruct); // do the init
+	GPIO_Init(&GPIOx, &currentPort_InitStruct); // do the init
+}
 
+uint8_t sameGPIO_TypeDef(GPIO_TypeDef* def1, GPIO_TypeDef* def2) {
+	return (def1->BSRRH == def2->BSRRH)
+			& (def1->BSRRL == def2->BSRRL)
+			& (def1->IDR == def2->IDR)
+			& (def1->LCKR == def2->LCKR)
+			& (def1->MODER == def2->MODER)
+			& (def1->ODR == def2->ODR)
+			& (def1->OSPEEDR == def2->OSPEEDR)
+			& (def1->OTYPER == def2->OTYPER)
+			& (def1->PUPDR == def2->PUPDR);
 }
